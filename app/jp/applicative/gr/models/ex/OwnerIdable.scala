e77d735f05035d941c0de58bc6803b134899a867
Tatsunori TAMAKI <github@qtamaki.com>
2014-11-05 15:45:45 +0900

    lazy

--- app/jp/applicative/gr/models/ex/OwnerIdable.scala
+++ app/jp/applicative/gr/models/ex/OwnerIdable.scala
@@ -6,7 +6,7 @@ trait OwnerIdable[A] {
   val q: QuerySQLSyntaxProvider[SQLSyntaxSupport[A], A]
   val owner_id: Option[Long]
   
-  val _sqls:SQLSyntax = owner_id match {
+  lazy val _sqls:SQLSyntax = owner_id match {
     case Some(o) => sqls.eq(q.ownerId, owner_id)
     case _ => sqls.isNull(q.ownerId)
   }
